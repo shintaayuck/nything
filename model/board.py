09cee0da32db046bf68a67660f29b4ca2df914fd
Fadhriga Bestari <fadhriga.bestari@gmail.com>
2018-09-18 15:17:43 +0700

    Create changes to board

--- model/board.py
+++ model/board.py
@@ -1,10 +1,12 @@
+import random
+import time
 from position import Position
 
 class Board :
 	'Chess board for solving n-ything problem'
-	
 	#constructor
-	def __init__(self, size = None) :
+	def __init__(self, size = None, white_pieces = None, black_pieces = None) :
+		random.seed(time.time())
 		self.size = size if size is not None else 8
 		self.matrix = []
 		self.white_pieces = []
@@ -12,7 +14,22 @@ class Board :
 		columns = [None] * self.size
 		for i in range(self.size) :
 			self.matrix.append(columns)
-	
+		for piece in white_pieces :
+			occupied = True
+			while occupied :
+				x = random.randint(0,self.size-1)
+				y = random.randint(0,self.size-1)
+				if(self.matrix[x][y] == None) :
+					occupied = False
+			self.matrix[x][y] = piece
+		for piece in black_pieces :
+			occupied = True
+			while occupied :
+				x = random.randint(0,self.size-1)
+				y = random.randint(0,self.size-1)
+				if(self.matrix[x][y] == None) :
+					occupied = False
+			self.matrix[x][y] = piece
 	#getters and setters
 	def get_size(self) :
 		return self.size
@@ -37,23 +54,19 @@ class Board :
 
 	def set_black_pieces(self, black_pieces) :
 		self.black_pieces = black_pieces
-
 	#method functions
 	#delete a piece from specific position
 	def delete_piece(self, position) :
 		result = self.matrix[position.get_x(), position.get_y()]
 		self.matrix[position.get_x(), position.get_y()] = None
 		return result
-	
 	#insert a piece to specific position
 	def insert_piece(self, position, piece) :
 		self.matrix[position.get_x(), position.get_y()] = piece
-	
 	#move a piece to specific position
 	def move_piece(self, init_position, goal_position) :
 		piece = delete_piece(init_position)
 		insert_piece(goal_position, piece)
-	
 	#count a piece's conflict
 	def count_conflict(self, possible_moves) :
 		result = 0
@@ -63,7 +76,6 @@ class Board :
 					result += 1
 					break
 		return result
-	
 	#count all pieces' conflict
 	def count_all_conflict(self) :
 		result = 0
@@ -74,3 +86,6 @@ class Board :
 			possible_moves = piece.show_possible_moves(piece.get_position().get_x(), piece.get_position().get_y())
 			result += count_conflict(possible_moves)
 		return result
+
+
+	
