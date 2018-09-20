ed84724395d3281b0e1ba78f27f201da49a1d6ff
Ricky Kennedy <13516105@std.stei.itb.ac.id>
2018-09-20 12:14:45 +0700

    init simulated annealing

---
+++ model/simulated_annealing.py
@@ -0,0 +1,16 @@
+from board import *
+from position import *
+import random
+
+def simulated_annealing(board):
+    
+
+def eval():
+
+
+def main():
+    rook = Rook()
+    print(rook.get_color())
+
+if __name__ == '__main__' :
+    main()
