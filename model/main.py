88bb19bdf8dfa9580156c81ccd29736b3bd61bf8
Fadhriga Bestari <fadhriga.bestari@gmail.com>
2018-09-22 18:13:46 +0700

    Change to board and hill climbing

--- model/main.py
+++ model/main.py
@@ -1,27 +1,25 @@
-from board import Board
-from rook import Rook
+import copy
 from position import Position
+from board import Board
+from bishop import Bishop
+from rook import Rook
 from queen import Queen
 from hill_climbing import *
 
 init_position = Position(0, 0)
 piece_1 = Rook(init_position, True)
 piece_2 = Rook(init_position, True)
-piece_3 = Rook(init_position, True)
+piece_3 = Bishop(init_position, True)
 piece_4 = Rook(init_position, True)
 piece_5 = Rook(init_position, True)
 piece_6 = Rook(init_position, True)
-piece_7 = Rook(init_position, True)
+piece_7 = Bishop(init_position, True)
 piece_8 = Rook(init_position, True)
 white_pieces = [piece_1, piece_2, piece_3,piece_4,piece_5,piece_6,piece_7,piece_8]
-board = Board(8, white_pieces, [])
-print(board.get_matrix())
-board.draw()
-print(board.count_all_conflict())
-for piece in board.get_white_pieces():
-    print(piece.get_position().get_x(), piece.get_position().get_y())
-hill_climbing(board)
-for piece in board.get_white_pieces():
-    print(piece.get_position().get_x(), piece.get_position().get_y())
-board.draw()
-print(board.count_all_conflict())
+black_pieces = copy.deepcopy(white_pieces)
+for piece in black_pieces :
+	piece.set_color(False)
+board = Board(8, white_pieces, black_pieces)
+for piece in board.get_white_pieces() :
+	print(piece.get_position().get_x(), piece.get_position().get_y())
+climb_hill(board)
